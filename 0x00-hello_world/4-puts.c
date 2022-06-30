c14d6a17ee354ce6f0b66305fdf0584e8b380be2
Shittu Temitope <danatech22@gmail.com>
2022-06-30 01:59:50 -0700

    Update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Description: Printing a string into a newline using the puts function
+  * Return: 0
+  */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle.\n");
+	return (0);
+}
