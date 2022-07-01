8ca192017db207714bcd63a5f2f856bae4c5ed3c
Shittu Temitope <danatech22@gmail.com>
2022-07-01 06:32:24 -0700

    Update

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+  * main - Entry point
+  * Description: Print alphabet in both lowercase and uppercase with put char
+  * Return: 0
+  */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for(ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
