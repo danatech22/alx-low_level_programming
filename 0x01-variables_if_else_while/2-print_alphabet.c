2683f0e0151a57acdc69a2482510d37140fd7107
Shittu Temitope <danatech22@gmail.com>
2022-07-01 06:23:45 -0700

    Update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+  * main - Entry point
+  * Description: Prints the alphabet in lower case
+  * Return: 0
+  */
+
+int main(void)
+{
+	int ch;
+
+
+	for ( ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
