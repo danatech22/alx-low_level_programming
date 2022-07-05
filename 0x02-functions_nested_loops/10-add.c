34a674acaf3b320dec1db907f7d3ef4d76b9e9e0
Shittu Temitope <danatech22@gmail.com>
2022-07-05 07:28:20 -0700

    Update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * add - Entry point
+  * @n: integer 1
+  * @m: integer 2
+  * Description: Add two integers together
+  * Return: sum
+  */
+
+int add(int n, int m)
+{
+	int sum;
+
+	sum = n + m;
+	return (sum);
+}
