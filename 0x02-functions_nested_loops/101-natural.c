1e13feac0d524c3f89dedf4305ef08f0cfdbf098
Shittu Temitope <danatech22@gmail.com>
2022-07-05 12:03:16 -0700

    Update

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,9 +9,10 @@ int main(void)
 {
 	int i, sum = 0;
 
-	if((i % 3 == 0) || (i % 5 == 0))
+	for (i = 0; i < 1024; i++)
 	{
-		sum += i;
+		if ((i % 3 == 0) || (i % 5 == 0))
+			sum += i;
 	}
 	printf("%d\n", sum);
 	return (0);
