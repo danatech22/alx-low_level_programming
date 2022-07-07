dd7ada7b616a9730def63af636afd55f27a6c576
Shittu Temitope <danatech22@gmail.com>
2022-07-07 04:56:37 -0700

    Update

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,4 +10,19 @@ void print_diagonal(int n)
 {
 	int len, space;
 
-	if (n 
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+		{
+			for (space = 0; space < len; space++)
+				_putchar(32);
+			_putchar('\\');
+
+			if (len == n - 1)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
