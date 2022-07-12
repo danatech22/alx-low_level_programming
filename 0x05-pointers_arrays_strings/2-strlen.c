3e0f3e1c60a34b89d19a56c7158dba28d7b9ff50
Shittu Temitope <danatech22@gmail.com>
2022-07-12 02:18:34 -0700

    Update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _strlen - Entry point
+  * Description: Returns the length of a string
+  * @s: the pointer variable
+  * Return: int
+  */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while(*s++)
+		len++;
+
+	return (len);
+}
