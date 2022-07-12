205c505ac586d1ed78a8c60053b2153aa5d7319e
Shittu Temitope <danatech22@gmail.com>
2022-07-12 03:36:21 -0700

    Update

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,18 +9,19 @@
 
 void rev_string(char *s)
 {
-	int len, i;
+	int len, i, half;
 	char tmp;
 
 	for (len = 0; s[len] != '\0'; len++)
 		;
 	i = 0;
+	half = len / 2;
 
-	while (len / 2)
+	while (half--)
 	{
 		tmp = s[len - i - 1];
 		s[len - i - 1] = s[i];
 		s[i] = tmp;
 		i++;
 	}
-}	
+}
