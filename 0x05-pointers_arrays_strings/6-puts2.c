838c5cdb214e5e788be2268753af1a4beea618cc
Shittu Temitope <danatech22@gmail.com>
2022-07-12 03:43:36 -0700

    Update

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * puts2 - Entry point
+  * Description: Prints the even character index
+  * @str - string used
+  * Return: void
+  */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+		_putchar(str[1]);
+	_putchar('\n');
+}
