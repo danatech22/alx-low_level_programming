cbf9166474954c8136f544c1028b4a08babef1b0
Shittu Temitope <danatech22@gmail.com>
2022-07-14 04:25:24 -0700

    Update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * cap_string - Capitalizes all words in a string
+  * *cap_string - Capitalizes all words in a string
   * @str: The string to be capitalized
   * Return: pointer
   */
