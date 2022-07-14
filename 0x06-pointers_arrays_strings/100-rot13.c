e1373c8461616686d15c97cde89e4e2d3938f655
Shittu Temitope <danatech22@gmail.com>
2022-07-14 06:57:17 -0700

    Update

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * *rot13 - Encodes a string
+  * rot13 - Encodes a string
   * @str: the string to be encoded
   * Return: A pointer
   */
@@ -9,28 +9,32 @@
 char *rot13(char *str)
 {
 	int i = 0, j;
-	char alph[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-			 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-			 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-			 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 
-			 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
-			 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
-			 'w', 'x', 'y', 'z'};
-	char r13k[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 
-			 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C',
-			 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 
-			 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 
-			 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 
-			 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-			 'j', 'k', 'l', 'm'};
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v', 
+			     'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+	                     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K', 
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w', 
+			     'x', 'y', 'z', 'a', 'b', 'c', 
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
 
 	while (str[i])
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (str[i] == alph[j])
+			if (str[i] == alphabet[j])
 			{
-				str[i] = r13k[j];
+				str[i] = rot13key[j];
 				break;
 			}
 		}
