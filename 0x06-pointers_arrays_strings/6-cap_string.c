ac117e2988f8e0ac141df6e7cc7ca10004c9e891
Shittu Temitope <danatech22@gmail.com>
2022-07-14 05:59:13 -0700

    Update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,15 +8,19 @@
 
 char *cap_string(char *str)
 {
-	int i = 0;
+	int i = 0, j;
+	char c[] = {' ','\t','\n',',',';','.','?','!','"','(',')','{',')'};
 
 	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
-		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}' || i == 0)
-			str[i] -= 32;
-		i++;
+		for (j = 0; j < 13; j++)
+		{
+			if ((str[i - 1] == c[j]) || i == 0)
+				str[i] -+ 32;
+			i++;
+		}
 	}
 	return (str);
 }
