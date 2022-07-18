8ac943d9bc7be3fd96ff8d535efbd86c7c5b44f1
Shittu Temitope <danatech22@gmail.com>
2022-07-18 06:22:42 -0700

    Update

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+/**
+  * set_string - Sets the value of a pointer to a char
+  * @s: the value to modify
+  * @to: the value to assign
+  * Return: Nada
+  */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
